c8e6b25c187a55bd27e892e59f233441b434e2ae
aniketroy <spacelover2003@gmail.com>
2022-05-22 01:06:07 +0530

    nothing

---
+++ TODO-List/scripts.js
@@ -0,0 +1,8 @@
+var addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", AddToDoItem);
+
+function AddToDoItem() {
+    alert("Add button clicked!");
+}
+
---
+++ html,css,js/test.py
@@ -0,0 +1,16 @@
+# import pywhatkit as kit
+# kit.sendwhatmsg("+919104546055", "yo bro",19,6)
+
+import pyautogui as pg
+import time
+
+time.sleep(1)
+for i in range(100):
+    pg.write("sweet dreams")
+    pg.press('Enter')
+
+# from sketchpy import library as lib
+    
+
+# obj = lib.apj()
+# obj.draw()
